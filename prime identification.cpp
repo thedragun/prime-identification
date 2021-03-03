36525b439e976f83960fa57ad1171d15f36c5a8d
Western Morris <270106785@yoobeestudent.ac.nz>
2021-03-04 12:55:43 +1300

    Add project files.

---
+++ prime identification.cpp
@@ -0,0 +1,32 @@
+// prime identification.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+
+
+    
+    int value = 3;
+
+    cout << "what is your number ";
+    cin >> value;
+
+
+    int equation = value / 2;
+
+        
+    if (equation == 1)
+
+    cout << "\n this number is a prime number";       
+
+    else
+
+    cout << "this number is Not a prime number";
+
+        
+}
